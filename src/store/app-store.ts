06681dfc988748e97cfbe59ee6b57024c580753a
khaoulalotfi <khaoula.lotfi@panko.lt>
2026-03-31 14:40:54 +0100

    Practicum 6 - UI setup

---
+++ src/store/app-store.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { createStore } from "zustand";
+import { createNavigationSlice } from "./navigation-slice";
+import { INavSlice, INotificationSlice } from "./store-t";
+import { createNotificationSlice } from "./notification-slice";
+
+export type IStoreState = INotificationSlice & INavSlice;
+
+export const appStore = () =>
+  createStore<IStoreState>()((...a) => ({
+    ...createNotificationSlice(...a),
+    ...createNavigationSlice(...a),
+  }));
---
+++ src/store/notification-slice.ts
@@ -0,0 +1,12 @@
+import { StateCreator } from "zustand";
+import { INotificationSlice } from "./store-t";
+
+export const createNotificationSlice: StateCreator<INotificationSlice> = (
+  set,
+) => ({
+  messages: [],
+  setMessage: (message) =>
+    set((state) => ({
+      messages: [...state.messages, message],
+    })),
+});
